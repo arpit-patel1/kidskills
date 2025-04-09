7f393c5950706180c9da19596ee65b650ceeb96e
Arpit Patel <ad1640@outlook.com>
2025-04-08 20:29:17 -0400

    Update ConfettiEffect component to reduce confetti pieces and adjust gravity for improved visual performance
    - Decreased the number of confetti pieces from 1500 to 500 to enhance rendering efficiency.
    - Adjusted gravity from 0.1 to 0.05 for a softer falling effect, improving the overall aesthetic of the confetti animation.

--- frontend/src/components/ConfettiEffect.js
+++ frontend/src/components/ConfettiEffect.js
@@ -11,10 +11,10 @@ const ConfettiEffect = ({ show, continuous = false }) => {
       <ReactConfetti
         width={window.innerWidth}
         height={window.innerHeight}
-        numberOfPieces={1500}
+        numberOfPieces={500}
         recycle={continuous}
         tweenDuration={4000}
-        gravity={0.1}
+        gravity={0.05}
         initialVelocityY={10}
         friction={0.97}
         colors={['#FF9FD5', '#ADD8E6', '#87CEEB', '#FFB6C1', '#B0E0E6', '#FFC0CB', '#1E90FF']}
